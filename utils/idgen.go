47c7f494f2690213097519f1d485835333958bd3
Alex Guerrieri <klaidliadon@gmail.com>
2017-03-08 12:18:45 +0100

    remove extra dash when making ids

--- utils/idgen.go
+++ utils/idgen.go
@@ -8,11 +8,13 @@ import (
 var (
 	cleanAlphaNum = regexp.MustCompile("[^[:alpha:]\\s\\d_]")
 	spaceTrim     = regexp.MustCompile("\\s+")
+	manyDash      = regexp.MustCompile("-+")
 )
 
 func MakeId(v string) string {
 	v = cleanAlphaNum.ReplaceAllString(v, " ")
 	v = spaceTrim.ReplaceAllString(v, "-")
+	v = manyDash.ReplaceAllString(v, "-")
 	v = strings.Trim(v, "-")
 	v = strings.ToLower(v)
 	return v
