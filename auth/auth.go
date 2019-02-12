a4ee3f39039c585d5a21bd8103312b93456b5a29
Alex Guerrieri <klaidliadon@gmail.com>
2019-02-12 18:03:59 +0100

    fix import

--- auth/auth.go
+++ auth/auth.go
@@ -9,8 +9,8 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/go-github/github"
-	"github.com/securityfirst/tent/models"
 	"golang.org/x/oauth2"
+	"github.com/securityfirst/tent/models"
 )
 
 const (
